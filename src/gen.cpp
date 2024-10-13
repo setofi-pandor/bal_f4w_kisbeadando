257850ce28efc254974787d1fd7ad6b03b14eb1c
setofi-pandor <balogh.levente.19@gmail.com>
2024-10-13 18:27:42 +0200

    generalas es osszeadas fajl valtoztatasa

--- src/gen.cpp
+++ src/gen.cpp
@@ -2,6 +2,7 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <cmath>  // Include for the sin() function
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32.hpp"
@@ -13,7 +14,7 @@ class GenSineAndRand : public rclcpp::Node
 public:
     GenSineAndRand(): Node("gen_node"), count_(0)
     {
-        RCLCPP_INFO(this->get_logger(), "Generating sine and random numbers");
+        RCLCPP_INFO(this->get_logger(), "Generating reflected sine and random numbers");
         pub1_ = this->create_publisher<std_msgs::msg::Float32>("sine", 10);
         pub2_ = this->create_publisher<std_msgs::msg::Float32>("rand", 10);
         timer_ = this->create_wall_timer(50ms, std::bind(&GenSineAndRand::timer_callback, this));
@@ -24,12 +25,17 @@ private:
     {
         auto message_sin = std_msgs::msg::Float32();
         auto message_rnd = std_msgs::msg::Float32();
-        message_sin.data = sin(count_++ / 50.0) * 100;
+        
+        // Reflected sine (y-axis mirrored): sin(-x)
+        message_sin.data = sin(-count_ / 50.0) * 100;  // Negate the argument to reflect the sine wave
         message_rnd.data = rand() % 5;
-        //   RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: '" << message_sin.data << "'");
+
         pub1_->publish(message_sin);
         pub2_->publish(message_rnd);
+
+        count_++;
     }
+
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub1_, pub2_;
     size_t count_;
--- src/sum.cpp
+++ src/sum.cpp
@@ -12,13 +12,22 @@ using namespace std::chrono_literals;
 class SumData : public rclcpp::Node
 {
 public:
-    SumData(): Node("sum_node"), count_(0)
+    SumData() : Node("sum_node"), count_(0), sine_(0.0), rand_(0.0), in_(0.0)
     {
         RCLCPP_INFO(this->get_logger(), "Summing numbers");
+
+        // Publisher to publish the sum of sine, rand, and in values
         pub1_ = this->create_publisher<std_msgs::msg::Float32>("out", 10);
-        sub1_ = this->create_subscription<std_msgs::msg::Float32>("sine", 10, std::bind(&SumData::sine_callback, this, _1));
-        sub2_ = this->create_subscription<std_msgs::msg::Float32>("rand", 10, std::bind(&SumData::rand_callback, this, _1));
-        sub3_ = this->create_subscription<std_msgs::msg::Float32>("in", 10, std::bind(&SumData::in_callback, this, _1));
+
+        // Subscriptions to sine, rand, and in topics
+        sub1_ = this->create_subscription<std_msgs::msg::Float32>(
+            "sine", 10, std::bind(&SumData::sine_callback, this, _1));
+        sub2_ = this->create_subscription<std_msgs::msg::Float32>(
+            "rand", 10, std::bind(&SumData::rand_callback, this, _1));
+        sub3_ = this->create_subscription<std_msgs::msg::Float32>(
+            "in", 10, std::bind(&SumData::in_callback, this, _1));
+
+        // Timer to periodically publish the sum
         timer_ = this->create_wall_timer(50ms, std::bind(&SumData::timer_callback, this));
     }
 
@@ -26,28 +35,34 @@ private:
     void timer_callback()
     {
         auto message_sum = std_msgs::msg::Float32();
+        // Summing sine, rand, and in values
         message_sum.data = sine_ + rand_ + in_;
         pub1_->publish(message_sum);
     }
+
     void sine_callback(const std_msgs::msg::Float32::SharedPtr msg)
     {
-        // RCLCPP_INFO(this->get_logger(), "Sine: '%f'", msg->data);
+        // Update sine value from the message
         sine_ = msg->data;
     }
+
     void rand_callback(const std_msgs::msg::Float32::SharedPtr msg)
     {
-        // RCLCPP_INFO(this->get_logger(), "Rand: '%f'", msg->data);
+        // Update rand value from the message
         rand_ = msg->data;
     }
+
     void in_callback(const std_msgs::msg::Float32::SharedPtr msg)
     {
-        // RCLCPP_INFO(this->get_logger(), "In: '%f'", msg->data);
+        // Update in value from the message (additional topic if needed)
         in_ = msg->data;
     }
+
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub1_;
     rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr sub1_, sub2_, sub3_;
-    double sum_, sine_, rand_, in_;
+    
+    double sine_, rand_, in_;
     size_t count_;
 };
 
